fccbc0a422714b9b306e99771a6ae3606f0bf0a5
RAVURI SREE SAI HARI KRISHNA <harirsskrishna@gmail.com>
2018-11-13 17:11:01 +0530

    Update HelloWorld/app/src/main/java/com/example/hari/helloworld/MainActivity.java

--- HelloWorld/app/src/main/java/com/example/hari/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/hari/helloworld/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.hari.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* This is the Brain of Main_Activity */
 public class MainActivity extends AppCompatActivity {
 
+/* This method will be called,when the Activity is created */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
